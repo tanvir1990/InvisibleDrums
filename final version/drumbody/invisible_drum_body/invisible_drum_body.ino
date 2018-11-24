c0a88345adeb94df5aa3212121612756a407e739
ZhiQiao761 <43482740+ZhiQiao761@users.noreply.github.com>
2018-11-24 16:19:00 -0500

    add flag to deal with multi-detection
    add flag to deal with multi-detection

--- final version/drumbody/invisible_drum_body/invisible_drum_body.ino
+++ final version/drumbody/invisible_drum_body/invisible_drum_body.ino
@@ -1,31 +1,30 @@
 // Saser Sensor code for Robojax.com
 #define DETECT 2 // pin 2 for  sensor
-#define ACTION 8 // pin 8 for action to do someting
-
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Invisible Drums");
   pinMode(DETECT, INPUT);//define detect input pin
-  pinMode(ACTION, OUTPUT);//define ACTION output pin
-
-  // Laser sensor code for Robojax.com
-
 }
 
 void loop() {
-  // Laser Sensor code for Robojax.com
-
-  int detected = digitalRead(DETECT);// read Laser sensor
-
-  if ( detected == HIGH)
-  {
+  int flag = 0;
+  while(1){
+    int detected = digitalRead(DETECT);// read Laser sensor
+  if ( detected == HIGH&&flag == 0){
     Serial.println((byte)1);
-  delay(100);
-    
-  } else {
-   Serial.println((byte)0);
-   delay(150);
-  }
- 
+    delay(100);
+  } else if  ( detected == HIGH&&flag == 1){
+    Serial.println((byte)1);
+    flag = 0;
+    delay(100);  
+  } else if  ( detected == LOW&&flag == 0){
+    Serial.println((byte)0);
+    flag = 1;
+    delay(135);  
+  } else if  ( detected == LOW&&flag == 1){
+    Serial.println((byte)1);
+    delay(100);
+  }  
+ }
 }
