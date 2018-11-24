c14868b16894f6da79004f13835d2b5b369ce9c6
ZhiQiao761 <43482740+ZhiQiao761@users.noreply.github.com>
2018-11-24 16:54:59 -0500

    the music play for PC
    replace  pygame module

--- Processor Pi/procRPi_playTone.py
+++ Processor Pi/procRPi_playTone.py
@@ -1,20 +1,25 @@
-isHit = True
+import serial
+
+ser = serial.Serial('/dev/ttyACM1',9600)
+tonename = "90000 clap.wav"
+snd = Sound() 
+snd.read(tonename)
 
 def playTone():
-    print ('Stream finished playing')
-    snd = Sound() 
-    snd.read('ex1.wav') 
-    snd.play()
-    if isHit == False:
-        print ('Stream not playing')
-    
-def resetisHit():
-    isHit = False
+    if isHit == 0:
+        print ('drum played') 
+        snd.play()
 
-if isHit:
+while 1:
+    v=ser.readline() //read the newest record
+
+    if v== b'0\r\n':
+        isHit = 0
+    else:
+        isHit = 1   
+
     playTone()
-    resetisHit()
-    print('Stream not playing')
+
   
 
     
