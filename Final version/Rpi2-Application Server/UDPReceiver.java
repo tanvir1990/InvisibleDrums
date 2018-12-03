d08a5885f10a62324b360cce13a4b9c23e4c68ac
ZichenWang03 <43318712+ZichenWang03@users.noreply.github.com>
2018-12-03 04:33:07 -0500

    Application Server Final Version

---
+++ Final version/Rpi2-Application Server/UDPReceiver.java
@@ -0,0 +1,50 @@
+//Author: Zhi Qiao(qiaoqiaojiushiwo@gmail.com)
+ 
+
+import java.net.*;
+
+public class UDPReceiver implements Runnable  {
+
+	private final static int PACKETSIZE = 100 ;
+	String message = "";
+	
+	public UDPReceiver(){
+		
+	}
+	public void applyreceive(int port) throws Exception{ 
+		// Convert the argument to ensure that is it valid
+		DatagramSocket socket = new DatagramSocket( port ) ;
+		System.out.println( "waiting for operation id " ) ;
+		receive(socket);
+		socket.close();
+	}
+	public void receive(DatagramSocket s) throws Exception{
+		DatagramPacket packet = new DatagramPacket( new byte[PACKETSIZE], PACKETSIZE ) ;
+		s.receive( packet ) ;
+		message = new String(packet.getData()).trim();
+		System.out.println("operation id: " + message ) ;
+	}
+	public static void main( String args[] ) throws Exception{
+		UDPReceiver receiver = new UDPReceiver();
+		receiver.applyreceive(1241);
+		
+	}
+	public String getMessage() {
+		return this.message;
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		UDPReceiver receiver = new UDPReceiver();
+		
+		try {
+			receiver.applyreceive(1236);
+			message = receiver.getMessage();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
+
---
+++ Final version/Rpi2-Application Server/playtone.py
@@ -0,0 +1,42 @@
+#Author: Zhi Qiao(qiaoqiaojiushiwo@gmail.com)
+import socket
+import pygame
+import serial
+
+#initiate Serial method port, read the record file
+ser = serial.Serial('/dev/ttyACM0',9600)
+#initiate the pygame module for music file play output
+pygame.init();
+#set the tone path and read the file
+pygame.mixer.music.load("/home/pi/Desktop/drumbodybody/invisibledrum/tone/tone.wav")
+#flag for signal received or not
+isHit = 1
+
+def playTone():
+    if isHit == 0:
+        print ('drum played')
+        #play the tone file
+        pygame.mixer.music.play()
+
+
+while 1:
+    #read the newest record 
+    v=ser.readline()
+    
+    if v== b'0\r\n':
+        isHit = 0
+    else:
+        isHit = 1   
+    playTone()
+    
+    
+	
+        
+
+
+
+    
+    
+
+
+  
