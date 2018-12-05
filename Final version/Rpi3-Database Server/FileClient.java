7e5d455c79ea94882f36ff258bf63df535153fba
ZichenWang03 <43318712+ZichenWang03@users.noreply.github.com>
2018-12-04 20:24:48 -0500

    Added javadoc for file client

--- Final version/Rpi3-Database Server/FileClient.java
+++ Final version/Rpi3-Database Server/FileClient.java
@@ -1,14 +1,26 @@
-//Author: Zhi Qiao(qiaoqiaojiushiwo@gmail.com)
- 
 
 import java.io.*;
 import java.net.*;
+
+/*
+ * FileClient is the sender for TCP file transfer
+ * 
+ * @author Zichen Wang
+ * @author Zhi Qiao
+ * @since 2018-12-03
+ */
 public class FileClient implements Runnable{
 	
+	
 	public FileClient() {
-		
+	// Empty constructor
 	}
 	
+	/* Sends the selected file into Stream
+	 *  
+	 * @param sr The Socket object used during transfer
+	 * @param path The path to store the file 
+	 */
 	public void send(Socket sr,String path) throws Exception {
 		FileInputStream fr= new FileInputStream(path);
 		byte b[]=new byte[40000];
@@ -18,6 +30,12 @@ public class FileClient implements Runnable{
 		fr.close();
 	}
 	
+	/*
+	 * Open the socket and call method send
+	 * 
+	 * @param port The port number thas going to be used
+	 * 
+	 */
 	public void applysend(String path,int port) throws Exception {
 		Socket sr=new Socket("10.0.0.71",port);
 		FileClient sender = new FileClient();
@@ -25,6 +43,11 @@ public class FileClient implements Runnable{
 		sr.close();
 	}
 	
+	/*
+	 * This is the man method that checks if this class runs without error.
+	 * 
+	 * @param args Unused
+	 */
 	
 	public static void main(String[] args) throws Exception{
 		Socket sr=new Socket("10.0.0.71",19123);
@@ -33,6 +56,10 @@ public class FileClient implements Runnable{
 		sr.close();
 		
 	}
+	
+	/*
+	 * This method is used for implemented threads
+	 */
 
 	@Override
 	public void run() {
